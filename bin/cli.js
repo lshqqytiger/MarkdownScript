671632d40b7572ce8fcc9a5877da0829afc50651
Seunghoon Lee <lshqqytiger@naver.com>
2023-08-08 03:57:55 +0900

    .

--- bin/cli.js
+++ bin/cli.js
@@ -5,53 +5,50 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
+const assert_1 = __importDefault(require("assert"));
 if (!process.argv[2])
-    throw new Error("Input file not specified");
-const input = fs_1.default.readFileSync(process.argv[2], "utf8");
+    throw new Error("Input file is not specified");
+const input = fs_1.default.readFileSync(path_1.default.resolve(".", process.argv[2]), "utf8");
 const output = process.argv[3];
 const functions = {};
 let stream = [];
 let compiled = "";
 stream = input.split("^f");
 compiled = "";
-for (let i of stream) {
+for (let i of stream)
     if (i.includes("f^")) {
-        const name = i.split("(")[0].replaceAll(" ", "");
-        const parameters = i
-            .split("(")[1]
-            .split(")")[0]
-            .split(",")
-            .map((v) => v.replaceAll(" ", ""));
-        functions[name] = new Function(...parameters, i.split(")")[1].split("f^")[0]);
+        const def = i.split("\n");
+        const header = def.shift();
+        (0, assert_1.default)(header !== undefined, "Function without header.");
+        const name = header.split("(")[0].replaceAll(" ", "");
+        functions[name] = new Function(header.match(/\(([^()]*)\)/)[1], def.join("\n").split("f^")[0]);
         append(i.split("f^")[1]);
     }
     else
         append(i);
-}
 stream = compiled.split("^-");
 compiled = "";
-for (const i of stream) {
+for (const i of stream)
     if (i.includes("-^")) {
         eval(`Object.assign(global, functions);\n${i.split("-^")[0]}`);
         append(i.split("-^")[1]);
     }
     else
         append(i);
-}
 stream = compiled.split("^(");
 compiled = "";
-for (const i of stream) {
+for (const i of stream)
     if (i.includes(")^")) {
         append(eval(`Object.assign(global, functions);\n${i.split(")^")[0]}`));
         append(i.split(")^")[1]);
     }
     else
         append(i);
-}
 fs_1.default.writeFileSync(output || process.argv[2].replace(".mds", ".md"), compiled);
 function append(text) {
     compiled += text;
 }
 function appendl(text) {
-    compiled += `${text}\n`;
+    append(`${text}\n`);
 }
--- src/cli.ts
+++ src/cli.ts
@@ -1,47 +1,44 @@
 #!/usr/bin/env node
 import fs from "fs";
+import path from "path";
+import assert from "assert";
 
-if (!process.argv[2]) throw new Error("Input file not specified");
+if (!process.argv[2]) throw new Error("Input file is not specified");
 
-const input = fs.readFileSync(process.argv[2], "utf8");
+const input = fs.readFileSync(path.resolve(".", process.argv[2]), "utf8");
 const output = process.argv[3];
-const functions: any = {};
-let stream = [];
+const functions: Record<string, Function> = {};
+let stream: string[] = [];
 let compiled = "";
 
 stream = input.split("^f");
 compiled = "";
-for (let i of stream) {
+for (let i of stream)
   if (i.includes("f^")) {
-    const name = i.split("(")[0].replaceAll(" ", "");
-    const parameters = i
-      .split("(")[1]
-      .split(")")[0]
-      .split(",")
-      .map((v) => v.replaceAll(" ", ""));
+    const def = i.split("\n");
+    const header = def.shift();
+    assert(header !== undefined, "Function without header.");
+    const name = header.split("(")[0].replaceAll(" ", "");
     functions[name] = new Function(
-      ...parameters,
-      i.split(")")[1].split("f^")[0]
+      header.match(/\(([^()]*)\)/)![1],
+      def.join("\n").split("f^")[0]
     );
     append(i.split("f^")[1]);
   } else append(i);
-}
 stream = compiled.split("^-");
 compiled = "";
-for (const i of stream) {
+for (const i of stream)
   if (i.includes("-^")) {
     eval(`Object.assign(global, functions);\n${i.split("-^")[0]}`);
     append(i.split("-^")[1]);
   } else append(i);
-}
 stream = compiled.split("^(");
 compiled = "";
-for (const i of stream) {
+for (const i of stream)
   if (i.includes(")^")) {
     append(eval(`Object.assign(global, functions);\n${i.split(")^")[0]}`));
     append(i.split(")^")[1]);
   } else append(i);
-}
 
 fs.writeFileSync(output || process.argv[2].replace(".mds", ".md"), compiled);
 
@@ -49,5 +46,5 @@ function append(text: string) {
   compiled += text;
 }
 function appendl(text: string) {
-  compiled += `${text}\n`;
+  append(`${text}\n`);
 }
